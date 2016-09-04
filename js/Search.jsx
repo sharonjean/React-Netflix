7b6b697bcdce013c39c9f37889d6e8ab4df250b2
Sharon Claitor <sclaitor@gmail.com>
2016-09-04 01:31:57 -0700

    Complete search bar logic

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,18 +2,32 @@ const React = require('react')
 const ShowCard = require('./ShowCard.jsx')
 const data = require('../public/data.json')
 
-const Search = () => (
-  <div className='container'>
-    <header className='header'>
-      <h1 className='brand'>NewFlix</h1>
-      <input className='search-input' type='text' placeholder='Search' />
-    </header>
-    <div className='shows'>
-      {data.shows.map((show) => (
-        <ShowCard {...show} key={show.imdbID} />
-      ))}
-    </div>
-  </div>
-)
+const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'Search'
+    }
+  },
+  handleSearchTermEvent (event) {
+    this.setState({ searchTerm: event.target.value })
+  },
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>NewFlix</h1>
+          <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search' onChange={this.handleSearchTermEvent} />
+        </header>
+        <div className='shows'>
+          {data.shows
+            .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map((show) => (
+              <ShowCard {...show} key={show.imdbID} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+})
 
 module.exports = Search
--- public/bundle.js
+++ public/bundle.js
@@ -25763,29 +25763,44 @@
 	var ShowCard = __webpack_require__(224);
 	var data = __webpack_require__(225);
 
-	var Search = function Search() {
-	  return React.createElement(
-	    'div',
-	    { className: 'container' },
-	    React.createElement(
-	      'header',
-	      { className: 'header' },
-	      React.createElement(
-	        'h1',
-	        { className: 'brand' },
-	        'NewFlix'
-	      ),
-	      React.createElement('input', { className: 'search-input', type: 'text', placeholder: 'Search' })
-	    ),
-	    React.createElement(
+	var Search = React.createClass({
+	  displayName: 'Search',
+	  getInitialState: function getInitialState() {
+	    return {
+	      searchTerm: 'Search'
+	    };
+	  },
+	  handleSearchTermEvent: function handleSearchTermEvent(event) {
+	    this.setState({ searchTerm: event.target.value });
+	  },
+	  render: function render() {
+	    var _this = this;
+
+	    return React.createElement(
 	      'div',
-	      { className: 'shows' },
-	      data.shows.map(function (show) {
-	        return React.createElement(ShowCard, _extends({}, show, { key: show.imdbID }));
-	      })
-	    )
-	  );
-	};
+	      { className: 'container' },
+	      React.createElement(
+	        'header',
+	        { className: 'header' },
+	        React.createElement(
+	          'h1',
+	          { className: 'brand' },
+	          'NewFlix'
+	        ),
+	        React.createElement('input', { value: this.state.searchTerm, className: 'search-input', type: 'text', placeholder: 'Search', onChange: this.handleSearchTermEvent })
+	      ),
+	      React.createElement(
+	        'div',
+	        { className: 'shows' },
+	        data.shows.filter(function (show) {
+	          return (show.title + ' ' + show.description).toUpperCase().indexOf(_this.state.searchTerm.toUpperCase()) >= 0;
+	        }).map(function (show) {
+	          return React.createElement(ShowCard, _extends({}, show, { key: show.imdbID }));
+	        })
+	      )
+	    );
+	  }
+	});
 
 	module.exports = Search;
 
