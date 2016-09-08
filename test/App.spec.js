6bf7033f033ac7497dab838ed1a93a9826f3c48d
Sharon Claitor <sclaitor@gmail.com>
2016-09-08 00:58:47 -0700

    Unit tests

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,5 +1,29 @@
 /* eslint-env mocha */
 
 const { expect } = require('chai')
+const React = require('react')
+const Search = require('../js/Search.jsx')
+const ShowCard = require('../js/ShowCard.jsx')
+const { shallow, mount } = require('enzyme')
+const { shows } = require('../public/data')
 
-describe('<Search />', )
+describe('<Search />', () => {
+  it('should render the brand', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.contains(<h1 className='brand'>NewFlix</h1>)).to.be.true
+  })
+
+  it('should render all the shows where data is present', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.find(ShowCard).length).to.equal(shows.length)
+  })
+
+  it('should filter correctly given new state', () => {
+    const wrapper = mount(<Search />)
+    const input = wrapper.find('.search-input')
+    input.node.value = 'house'
+    input.simulate('change')
+    expect(wrapper.state('searchTerm')).to.equal('house')
+    expect(wrapper.find('.show-card').length).to.equal(2)
+  })
+})
